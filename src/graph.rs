4134c124263291f7b8a547ec05581dab865a37a2
Kerry Wang <kerrywang369@gmail.com>
2023-10-26 19:04:38 -0600

    too hard

--- src/graph.rs
+++ src/graph.rs
@@ -42,6 +42,104 @@ mod tests {
     use petgraph::dot::{Config, Dot};
     use petgraph::graph::{DiGraph, NodeIndex, UnGraph};
 
+    fn make_basic_branch() -> DiGraph<Element, ()> {
+        let mut graph: DiGraph<Element, ()> = DiGraph::new();
+
+        // prev = graph.add_node(AssignNode(i, Int(0)))
+        // prev = ifelse = graph.add_node(BranchNode(BinOp(i, "<", n)), prev)
+
+        // prev = postifelse = graph.add_node(AssignNode(n, i))
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, Int(10)), prev)
+
+        // prev = graph.add_node(TrueNode(), ifelse)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, Int(1)), prev)
+        // prev = graph.add_node(AssignNode(i, Int(2)), prev)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, Int(3)), prev, children=[postifelse])
+
+        // prev = graph.add_node(FalseNode(), ifelse)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, Int(1)), prev)
+        // prev = graph.add_node(AssignNode(i, Int(2)), prev)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, i), prev)
+        // prev = graph.add_node(AssignNode(i, Int(4)), prev, children=[postifelse])
+
+        // return graph
+
+        let node0 = graph.add_node(Element::Assign(Assign {
+            lvalue: "i".to_string(),
+            rvalue: "0".to_string(),
+            unique_id: "0".to_string(),
+        }));
+
+        let node1 = graph.add_node(Element::Branch(Branch {
+            cond: "i < n".to_string(),
+            unique_id: "1".to_string(),
+        }));
+        graph.add_edge(node0, node1, ());
+
+        let node2 = graph.add_node(Element::Assign(Assign {
+            lvalue: "n".to_string(),
+            rvalue: "i".to_string(),
+            unique_id: "2".to_string(),
+        }));
+        graph.add_edge(node1, node2, ());
+
+        let node3 = graph.add_node(Element::Assign(Assign {
+            lvalue: "i".to_string(),
+            rvalue: "i".to_string(),
+            unique_id: "3".to_string(),
+        }));
+        graph.add_edge(node2, node3, ());
+
+        let node4 = graph.add_node(Element::Assign(Assign {
+            lvalue: "i".to_string(),
+            rvalue: "10".to_string(),
+            unique_id: "4".to_string(),
+        }));
+        graph.add_edge(node3, node4, ());
+
+        let node5 = graph.add_node(Element::BlockHead(BlockHead {
+            phi: Default::default(),
+            unique_id: "5".to_string(),
+        }));
+        graph.add_edge(node1, node5, ());
+
+        let node6 = graph.add_node(Element::Assign(Assign {
+            lvalue: "i".to_string(),
+            rvalue: "i".to_string(),
+            unique_id: "6".to_string(),
+        }));
+        graph.add_edge(node5, node6, ());
+
+        let node7 = graph.add_node(Element::Assign(Assign {
+            lvalue: "i".to_string(),
+            rvalue: "i".to_string(),
+            unique_id: "7".to_string(),
+        }));
+        graph.add_edge(node6, node7, ());
+
+        let node8 = graph.add_node(Element::Assign(Assign {
+            lvalue: "i".to_string(),
+            rvalue: "1".to_string(),
+            unique_id: "8".to_string(),
+        }));
+
+        let node9 = graph.add_node(Element::Assign(Assign {
+            lvalue: "i".to_string(),
+            rvalue: "2".to_string(),
+            unique_id: "9".to_string(),
+        }));
+
+        graph
+    }
+
     #[test]
     fn test_petgraph() {
         let mut g: DiGraph<Element, ()> = DiGraph::new();
@@ -67,7 +165,16 @@ mod tests {
         let node3 = g.add_node(Element::BlockHead(blockHead1));
 
         // Add edges if needed
-        // g.add_edge(node1, node2, "Edge data here");
+        g.add_edge(node1, node2, ());
+        g.add_edge(node2, node3, ());
+
+        let children: Vec<_> = g
+            .neighbors_directed(node2, petgraph::Direction::Outgoing)
+            .collect();
+        
+        // Get Node with id 1
+        let node = g.node_weight(NodeIndex::new(1)).unwrap();
+        println!("{:?}", children);
 
         // Print the graph using Dot
         println!("{:?}", Dot::new(&g));
