0544a750f2de33c6a3a640ec9debee4bc6571202
Kerry Wang <kerrywang369@gmail.com>
2023-11-06 08:51:47 -0700

    minor

--- crates/frontend/src/parser.rs
+++ crates/frontend/src/parser.rs
@@ -3,39 +3,57 @@ use core::panic;
 use rustpython_parser::{ast, Parse};
 use tohdl_ir::graph::DiGraph;
 
-pub fn parse_func(text: &str) -> DiGraph {
-    let ast = ast::Suite::parse(text, "<embedded>");
-
-    println!("{:#?}", ast.as_ref().unwrap());
-
-    let binding = ast.unwrap();
-    let func = binding.get(0).unwrap();
-
-    let body = match func {
-        ast::Stmt::FunctionDef(ast::StmtFunctionDef { body, .. }) => body,
-        _ => panic!("Not a function"),
-    };
-
-    let graph = DiGraph::new();
-
-    for stmt in body {
-        recurse_stmt(stmt);
-    }
-
-    graph
+struct Parser {
+    graph: DiGraph,
 }
 
-fn recurse_stmt(stmt: &ast::Stmt) {
-    match stmt {
-        ast::Stmt::Assign(ast::StmtAssign {
-            range,
-            targets,
-            value,
-            type_comment,
-        }) => {
-            println!("Assign {:?} = {:?}", targets, value);
+impl Parser {
+    pub fn new() -> Self {
+        Self {
+            graph: DiGraph::new(),
         }
-        _ => {}
+    }
+
+    pub fn parse_func(&self, text: &str) -> DiGraph {
+        let ast = ast::Suite::parse(text, "<embedded>");
+
+        println!("{:#?}", ast.as_ref().unwrap());
+
+        let binding = ast.unwrap();
+        let func = binding.get(0).unwrap();
+
+        let body = match func {
+            ast::Stmt::FunctionDef(ast::StmtFunctionDef { body, .. }) => body,
+            _ => panic!("Not a function"),
+        };
+
+        let graph = DiGraph::new();
+
+        for stmt in body {
+            self.parse_stmt(stmt);
+        }
+
+        graph
+    }
+
+    fn parse_stmt(&self, stmt: &ast::Stmt) {
+        match stmt {
+            ast::Stmt::Assign(ast::StmtAssign {
+                range,
+                targets,
+                value,
+                type_comment,
+            }) => {
+                println!("Assign {:?} = {:?}", targets, value);
+                let name = match targets.get(0).unwrap() {
+                    ast::Expr::Name(ast::ExprName { range, id, ctx }) => id.as_str(),
+                    _ => panic!(),
+                };
+
+                println!("name {}", name);
+            }
+            _ => {}
+        }
     }
 }
 
@@ -53,6 +71,7 @@ def is_odd(n):
         yield i
     return
 "#;
-        let ast = parse_func(python_source);
+        let parser = Parser::new();
+        let ast = parser.parse_func(python_source);
     }
 }
