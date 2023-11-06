39cdbe2791d10f8c3271606ebab972511022c462
Kerry Wang <kerrywang369@gmail.com>
2023-11-06 01:36:58 -0700

    assign

--- crates/frontend/src/parser.rs
+++ crates/frontend/src/parser.rs
@@ -17,7 +17,26 @@ pub fn parse_func(text: &str) -> DiGraph {
     };
 
     let graph = DiGraph::new();
+
+    for stmt in body {
+        recurse_stmt(stmt);
+    }
+
     graph
+}
+
+fn recurse_stmt(stmt: &ast::Stmt) {
+    match stmt {
+        ast::Stmt::Assign(ast::StmtAssign {
+            range,
+            targets,
+            value,
+            type_comment,
+        }) => {
+            println!("Assign {:?} = {:?}", targets, value);
+        }
+        _ => {}
+    }
 }
 
 #[cfg(test)]
@@ -27,8 +46,12 @@ mod tests {
     #[test]
     fn test_parse() {
         let python_source = r#"
-def is_odd(i):
-    return bool(i & 1)
+def is_odd(n):
+    i = 0
+    if i < n:
+        i = i + 1
+        yield i
+    return
 "#;
         let ast = parse_func(python_source);
     }
