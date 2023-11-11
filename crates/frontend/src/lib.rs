9d386914582d43f623767dd43cc9f2b6c6486f1f
Kerry Wang <kerrywang369@gmail.com>
2023-11-11 14:07:26 -0700

    rustpython ast

--- crates/frontend/src/lib.rs
+++ crates/frontend/src/lib.rs
@@ -2,6 +2,10 @@ pub mod parser;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use ast::StmtIf;
+    // use crate::visitor::Visitor;
+    use rustpython_parser::ast::Visitor;
     use rustpython_parser::{ast, Parse};
 
     #[test]
@@ -16,5 +20,34 @@ def is_odd(i):
 
         // Print AST
         println!("{:#?}", ast.unwrap());
+    }
+
+    #[test]
+    fn test_visitor() {
+        let python_source = r#"
+def rectangle(m, n):
+    i = 0
+    while i < m:
+        j = 0
+        while j < n:
+            if i % 2:
+                yield i, j
+            j = j + 1
+        print()
+        i = i + 1
+"#;
+        struct MyVisitor;
+        impl Visitor for MyVisitor {
+            fn visit_stmt_if(&mut self, node: StmtIf) {
+                println!("{:?}", node);
+                self.generic_visit_stmt_if(node)
+            }
+        }
+
+        let mut visitor = MyVisitor;
+        let ast = ast::Suite::parse(python_source, "<embedded>").unwrap();
+
+        // println!("{:#?}", ast);
+        visitor.visit_stmt(ast[0].clone());
     }
 }
