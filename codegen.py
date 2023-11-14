434847861b5799d44e30f7f2ad9b63132ae60586
Kerry Wang <kerrywang369@gmail.com>
2023-11-14 16:02:25 -0700

    codegen seems good

--- codegen.py
+++ codegen.py
@@ -1,66 +1,66 @@
-def func0(n00, i100, i1000, n000, i10000):
+def func0(n00):
     i00 = 0
     i10 = i00
     if (i10 < n00):
-        if (i1000 % 2):
-            yield i1000
-            yield from func1(i1000, n000, i10000)
+        if (i10 % 2):
+            yield i10
+            yield from func1(i10, n00)
         else:
-            i2000 = (i1000 + 1)
-            i11 = i2000
+            i20 = (i10 + 1)
+            i11 = i20
             if (i11 < n00):
-                if (i1000 % 2):
-                    yield i1000
-                    yield from func1(i1000, n000, i10000)
+                if (i11 % 2):
+                    yield i11
+                    yield from func1(i11, n00)
                 else:
-                    yield from func3(i100, n00, i1000)
+                    yield from func3(i11, n00)
             else:
                 return 0
     else:
         return 0
 
-def func1(i100, n00, i1000, n000, i10000):
-    i200 = (i100 + 1)
-    i10 = i200
-    if (i10 < n00):
-        if (i1000 % 2):
-            yield i1000
-            yield from func1(i1000, n000, i10000)
+def func1(i10, n00):
+    i20 = (i10 + 1)
+    i11 = i20
+    if (i11 < n00):
+        if (i11 % 2):
+            yield i11
+            yield from func1(i11, n00)
         else:
-            i2010 = (i1000 + 1)
-            yield from func2(i2010, n000, i1000, i10000)
+            i21 = (i11 + 1)
+            yield from func2(i20, n00)
     else:
         return 0
 
-def func2(i10, n00, i100, i1000, n000, i10000):
+def func2(i10, n00):
     if (i10 < n00):
-        if (i1000 % 2):
-            yield i1000
-            yield from func1(i1000, n000, i10000)
+        if (i10 % 2):
+            yield i10
+            yield from func1(i10, n00)
         else:
-            i2000 = (i1000 + 1)
-            i11 = i2000
+            i20 = (i10 + 1)
+            i11 = i20
             if (i11 < n00):
-                if (i1000 % 2):
-                    yield i1000
-                    yield from func1(i1000, n000, i10000)
+                if (i11 % 2):
+                    yield i11
+                    yield from func1(i11, n00)
                 else:
-                    yield from func3(i100, n00, i1000)
+                    yield from func3(i11, n00)
             else:
                 return 0
     else:
         return 0
 
-def func3(i100, n00, i1000, n000, i10000):
-    i200 = (i100 + 1)
-    i10 = i200
-    if (i10 < n00):
-        if (i1000 % 2):
-            yield i1000
-            yield from func1(i1000, n000, i10000)
+def func3(i10, n00):
+    i20 = (i10 + 1)
+    i11 = i20
+    if (i11 < n00):
+        if (i11 % 2):
+            yield i11
+            yield from func1(i11, n00)
         else:
-            i2010 = (i1000 + 1)
-            yield from func2(i2010, n000, i1000, i10000)
+            i21 = (i11 + 1)
+            yield from func2(i20, n00)
     else:
         return 0
 
