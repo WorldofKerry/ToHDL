d294d2f2783833da4b464064e21df132891a592d
Kerry Wang <kerrywang369@gmail.com>
2023-11-20 18:13:18 -0700

    codegen seems good

--- codegen.py
+++ codegen.py
@@ -1,55 +1,53 @@
-def func0():
+def func0(n00):
+    i00 = 0
     a00 = 0
-    if a00 > 1:
-        b00 = 10
-        b11 = b00
-        yield a00
-        yield from func1(b11, a00)
+    b00 = 1
+    a10 = a00
+    b10 = b00
+    i10 = i00
+    if a10 < n00:
+        if a10 % 2:
+            yield a10
+            temp20 = a10 + b10
+            a20 = b10
+            b20 = temp20
+            i20 = i10 + 1
+            yield from func1(a20, b20, i20, n00)
+        else:
+            temp21 = a10 + b10
+            a21 = b10
+            b21 = temp21
+            i21 = i10 + 1
+            yield from func1(a21, b21, i21, n00)
     else:
-        b40 = 11
-        yield b40
-        b10 = b40
-        yield from func5(a00, b10)
+        yield 0
 
 
-def func1(b10, a00, a000, b100):
-    yield b10
-    if b10 % 10:
-        yield from func4(a00, b10)
+def func1(a10, b10, i10, n00):
+    if a10 < n00:
+        if a10 % 2:
+            yield a10
+            temp20 = a10 + b10
+            a20 = b10
+            b20 = temp20
+            i20 = i10 + 1
+            yield from func1(a20, b20, i20, n00)
+        else:
+            temp21 = a10 + b10
+            a21 = b10
+            b21 = temp21
+            i21 = i10 + 1
+            yield from func1(a21, b21, i21, n00)
     else:
-        b30 = a00 + 2
-        a20 = a00
-        b20 = b30
-        yield from func2(a20, b20)
-
-
-def func2(a20, b20):
-    yield a20
-    yield from func3(b20)
-
-
-def func3(b20):
-    yield b20
-
-
-def func4(a00, b10):
-    yield a00
-    a10 = 15
-    a20 = a10
-    b20 = b10
-    yield from func2(a20, b20)
-
-
-def func5(a00, b10):
-    yield a00
-    yield from func1(b10, a00)
+        yield 0
 
 
 import itertools
 
 
 def main():
-    inputs = tuple()
+    # inputs = tuple()
+    inputs = (100,)
     gen = func0(*inputs)
     for val in itertools.islice(gen, 500):
         print(val, end=", ")
