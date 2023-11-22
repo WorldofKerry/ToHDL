99060b430a211c4fc1501236b531764f0a41e080
Kerry Wang <kerrywang369@gmail.com>
2023-11-21 17:58:20 -0700

    double-pass seems good

--- crates/passes/src/transform/braun_et_al.rs
+++ crates/passes/src/transform/braun_et_al.rs
@@ -10,31 +10,37 @@ use tohdl_ir::graph::*;
 pub struct BraunEtAl {
     result: TransformResultType,
     current_def: BTreeMap<VarExpr, BTreeMap<NodeIndex, VarExpr>>,
-    pub(crate) graph: CFG,
     var_counter: BTreeMap<VarExpr, usize>,
+    read_vars: BTreeMap<NodeIndex, Vec<VarExpr>>,
+    wrote_vars: BTreeMap<NodeIndex, Vec<VarExpr>>,
 }
 
 impl BraunEtAl {
     pub(crate) fn write_variable(
         &mut self,
+        graph: &mut CFG,
         variable: &VarExpr,
         block: &NodeIndex,
         value: &VarExpr,
     ) {
         println!("write variable {} {} {}", variable, block, value);
 
-        let block_head_idx = self.get_block_head(*block);
+        let block_head_idx = self.get_block_head(graph, *block);
 
         // Create var map if it doesn't exist
         let var_map = match self.current_def.entry(variable.clone()) {
             Entry::Occupied(o) => o.into_mut(),
             Entry::Vacant(v) => v.insert(BTreeMap::new()),
         };
-
         var_map.insert(block_head_idx, value.clone());
     }
 
-    pub(crate) fn read_variable(&mut self, variable: &VarExpr, block: &NodeIndex) -> VarExpr {
+    pub(crate) fn read_variable(
+        &mut self,
+        graph: &mut CFG,
+        variable: &VarExpr,
+        block: &NodeIndex,
+    ) -> VarExpr {
         println!("read variable {} {}", block, variable);
         if !self.current_def.contains_key(variable) {
             self.current_def.insert(variable.clone(), BTreeMap::new());
@@ -44,42 +50,43 @@ impl BraunEtAl {
             self.current_def[variable][block].clone()
         } else {
             // global value numbering
-            self.read_variable_recursive(variable, block)
+            self.read_variable_recursive(graph, variable, block)
         }
     }
 
     pub(crate) fn read_variable_recursive(
         &mut self,
+        graph: &mut CFG,
         variable: &VarExpr,
         block: &NodeIndex,
     ) -> VarExpr {
         println!("read variable recursive {} {}", block, variable);
         // assume complete CFG
         let val;
-        let preds = self.graph.pred(*block).collect::<Vec<_>>();
+        let preds = graph.pred(*block).collect::<Vec<_>>();
         if preds.len() == 1 {
-            val = self.read_variable(variable, &preds[0]);
+            val = self.read_variable(graph, variable, &preds[0]);
         } else {
             // break potential cycles with operandless phi
-            val = self.new_phi(block, variable); // add new phi to this block
-            self.write_variable(variable, block, &val);
-            self.add_phi_operands(block, variable);
-            self.write_variable(variable, block, &val);
+            val = self.new_phi(graph, block, variable); // add new phi to this block
+            self.write_variable(graph, variable, block, &val);
+            self.add_phi_operands(graph, block, variable);
+            self.write_variable(graph, variable, block, &val);
         }
         val
     }
 
     /// Given a node, get its block head
-    pub(crate) fn get_block_head(&self, node: NodeIndex) -> NodeIndex {
+    pub(crate) fn get_block_head(&self, graph: &mut CFG, node: NodeIndex) -> NodeIndex {
         let mut cur = node;
         loop {
-            let preds = self.graph.pred(cur).collect::<Vec<_>>();
+            let preds = graph.pred(cur).collect::<Vec<_>>();
             // If node has zero preds or multiple preds (e.g. func node)
             if preds.len() == 0 || preds.len() > 1 {
                 return cur;
             }
             // If node's pred  has multiple succs (e.g. branch node)
-            if self.graph.succ(preds[0]).collect::<Vec<_>>().len() > 1 {
+            if graph.succ(preds[0]).collect::<Vec<_>>().len() > 1 {
                 return cur;
             }
             cur = preds[0];
@@ -87,7 +94,7 @@ impl BraunEtAl {
     }
 
     /// Adds a new phi variable
-    pub(crate) fn new_phi(&mut self, block: &NodeIndex, var: &VarExpr) -> VarExpr {
+    pub(crate) fn new_phi(&mut self, graph: &mut CFG, block: &NodeIndex, var: &VarExpr) -> VarExpr {
         let count = *self.var_counter.get(var).unwrap_or(&0);
         self.var_counter.insert(var.clone(), count + 1);
 
@@ -95,9 +102,9 @@ impl BraunEtAl {
         let name = format!("{}.{}", var.name, count);
         let new_var = VarExpr::new(&name);
 
-        let block_head_idx = self.get_block_head(block.clone());
+        let block_head_idx = self.get_block_head(graph, block.clone());
         if let Some(FuncNode { params }) =
-            FuncNode::concrete_mut(self.graph.get_node_mut(block_head_idx))
+            FuncNode::concrete_mut(graph.get_node_mut(block_head_idx))
         {
             params.push(new_var.clone())
         } else {
@@ -109,12 +116,13 @@ impl BraunEtAl {
         new_var
     }
 
-    pub(crate) fn add_phi_operands(&mut self, block: &NodeIndex, var: &VarExpr) {
+    pub(crate) fn add_phi_operands(&mut self, graph: &mut CFG, block: &NodeIndex, var: &VarExpr) {
         println!("add phi operands {} {}", block, var);
-        let block_head_idx = self.get_block_head(block.clone());
-        for pred in self.graph.pred(block_head_idx).collect::<Vec<_>>() {
-            let arg = self.read_variable(var, &self.get_block_head(pred)).clone();
-            if let Some(CallNode { args }) = CallNode::concrete_mut(self.graph.get_node_mut(pred)) {
+        let block_head_idx = self.get_block_head(graph, block.clone());
+        for pred in graph.pred(block_head_idx).collect::<Vec<_>>() {
+            let pred_head_idx = &self.get_block_head(graph, pred);
+            let arg = self.read_variable(graph, var, pred_head_idx).clone();
+            if let Some(CallNode { args }) = CallNode::concrete_mut(graph.get_node_mut(pred)) {
                 args.push(arg)
             } else {
                 println!(
@@ -129,21 +137,33 @@ impl BraunEtAl {
 impl Transform for BraunEtAl {
     fn apply(&mut self, graph: &mut CFG) -> &TransformResultType {
         // TODO: change API to store a reference
-        self.graph = graph.clone();
-
-        let node_indexes = self.graph.nodes().collect::<Vec<_>>();
+        let node_indexes = graph.nodes().collect::<Vec<_>>();
         for idx in &node_indexes {
-            let node = self.graph.get_node_mut(*idx);
-            for exprs in node.read_exprs_mut() {
-                for var in exprs.get_vars_iter() {
-                    *var = self.read_variable(var, idx);
-                }
+            let mut read_vars = match self.read_vars.entry(*idx) {
+                Entry::Occupied(o) => o.into_mut(),
+                Entry::Vacant(v) => v.insert(vec![]),
+            }
+            .clone();
+            let mut wrote_vars = match self.wrote_vars.entry(*idx) {
+                Entry::Occupied(o) => o.into_mut(),
+                Entry::Vacant(v) => v.insert(vec![]),
+            }
+            .clone();
+            let mut node = graph.get_node(*idx).clone();
+            for var in node.referenced_vars() {
+                let new_var = self.read_variable(graph, var, idx);
+                read_vars.push(new_var);
             }
             for var in node.defined_vars_mut() {
-                self.write_variable(var, idx, var);
+                self.write_variable(graph, var, idx, var);
             }
+            self.read_vars.insert(*idx, read_vars);
+            self.wrote_vars.insert(*idx, wrote_vars);
         }
-        *graph = self.graph.clone();
+
+        println!("read_vars {:?}", self.read_vars);
+        println!("wrote_vars {:?}", self.wrote_vars);
+        println!("current_def {:#?}", self.current_def);
         &self.result
     }
 }
@@ -162,16 +182,19 @@ pub mod tests {
         insert_call::InsertCallNodes::default().apply(&mut graph);
 
         let mut pass = BraunEtAl::default();
-        pass.graph = graph;
+        assert_eq!(pass.get_block_head(&mut graph, 4.into()), 6.into());
 
-        assert_eq!(pass.get_block_head(4.into()), 6.into());
-
-        pass.write_variable(&VarExpr::new("i"), &1.into(), &VarExpr::new("i0"));
-        let result = pass.read_variable(&VarExpr::new("i"), &2.into());
+        pass.write_variable(
+            &mut graph,
+            &VarExpr::new("i"),
+            &1.into(),
+            &VarExpr::new("i0"),
+        );
+        let result = pass.read_variable(&mut graph, &VarExpr::new("i"), &2.into());
 
         println!("result {}", result);
 
-        write_graph(&pass.graph, "braun.dot");
+        write_graph(&graph, "braun.dot");
     }
 
     #[test]
@@ -182,22 +205,31 @@ pub mod tests {
         insert_call::InsertCallNodes::default().apply(&mut graph);
 
         let mut pass = BraunEtAl::default();
-        pass.graph = graph;
 
-        assert_eq!(pass.get_block_head(1.into()), 0.into());
-        assert_eq!(pass.get_block_head(6.into()), 2.into());
-        assert_eq!(pass.get_block_head(4.into()), 5.into());
+        assert_eq!(pass.get_block_head(&mut graph, 1.into()), 0.into());
+        assert_eq!(pass.get_block_head(&mut graph, 6.into()), 2.into());
+        assert_eq!(pass.get_block_head(&mut graph, 4.into()), 5.into());
 
-        let result = pass.read_variable(&VarExpr::new("a"), &0.into());
+        let result = pass.read_variable(&mut graph, &VarExpr::new("a"), &0.into());
         println!("result {}", result);
-        let result = pass.read_variable(&VarExpr::new("a"), &1.into());
+        let result = pass.read_variable(&mut graph, &VarExpr::new("a"), &1.into());
         println!("result {}", result);
-        pass.write_variable(&VarExpr::new("b"), &3.into(), &VarExpr::new("a"));
-        pass.write_variable(&VarExpr::new("b"), &2.into(), &VarExpr::new("1"));
-        let result = pass.read_variable(&VarExpr::new("b"), &4.into());
+        pass.write_variable(
+            &mut graph,
+            &VarExpr::new("b"),
+            &3.into(),
+            &VarExpr::new("a"),
+        );
+        pass.write_variable(
+            &mut graph,
+            &VarExpr::new("b"),
+            &2.into(),
+            &VarExpr::new("1"),
+        );
+        let result = pass.read_variable(&mut graph, &VarExpr::new("b"), &4.into());
         println!("result {}", result);
 
-        write_graph(&pass.graph, "braun.dot");
+        write_graph(&graph, "braun.dot");
     }
 
     #[test]
@@ -208,13 +240,14 @@ pub mod tests {
         insert_call::InsertCallNodes::default().apply(&mut graph);
 
         let mut pass = BraunEtAl::default();
-        pass.graph = graph;
 
-        assert_eq!(pass.get_block_head(1.into()), 0.into());
-        assert_eq!(pass.get_block_head(6.into()), 2.into());
-        assert_eq!(pass.get_block_head(4.into()), 5.into());
+        assert_eq!(pass.get_block_head(&mut graph, 1.into()), 0.into());
+        assert_eq!(pass.get_block_head(&mut graph, 6.into()), 2.into());
+        assert_eq!(pass.get_block_head(&mut graph, 4.into()), 5.into());
 
-        write_graph(&pass.graph, "braun.dot");
+        pass.apply(&mut graph);
+
+        write_graph(&graph, "braun.dot");
     }
 
     #[test]
