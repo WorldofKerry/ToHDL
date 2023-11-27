6e72c411731921258886d32b718d3ec8c1a8f9bb
Kerry Wang <kerrywang369@gmail.com>
2023-11-26 22:28:24 -0700

    basic nodes working

--- crates/codegen/src/python.rs
+++ crates/codegen/src/python.rs
@@ -175,29 +175,9 @@ impl CodeGen {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::tests::{make_odd_fib, make_yields};
+    use tohdl_ir::graph::CFG;
     use tohdl_passes::{manager::PassManager, optimize::*, transform::*, Transform};
-
-    pub fn make_odd_fib() -> CFG {
-        let code = r#"
-def even_fib():
-    i = 0
-    a = 0
-    b = 1
-    while a < n:
-        if a % 2:
-            yield a
-        temp = a + b
-        a = b
-        b = temp
-        i = i + 1
-    yield 123
-"#;
-        let visitor = tohdl_frontend::AstVisitor::from_text(code);
-
-        let graph = visitor.get_graph();
-
-        graph
-    }
 
     #[test]
     fn odd_fib() {
@@ -231,20 +211,6 @@ def even_fib():
             let code = codegen.get_code();
             println!("{}", code);
         }
-    }
-
-    pub fn make_yields() -> CFG {
-        let code = r#"
-def even_fib():
-    yield n + 1
-    yield n + 2
-    yield n + 3
-"#;
-        let visitor = tohdl_frontend::AstVisitor::from_text(code);
-
-        let graph = visitor.get_graph();
-
-        graph
     }
 
     #[test]
