257bbce33f00a891e8647051541978d093a40280
Kerry Wang <kerrywang369@gmail.com>
2023-12-19 13:45:12 -0700

    good????

--- build.sh
+++ build.sh
@@ -1,24 +1,31 @@
 #! /bin/bash -ex
-setup_venv() {
-    python3 -m venv venv
-    source venv/bin/activate
+
+dep() {
     python3 -m pip install -e .[dev]
-
-    # Re-activate for pytest
-    deactivate && source venv/bin/activate
 }
 
-build_rust() {
-    maturin develop --manifest-path crates/pytohdl/Cargo.toml
-}
-
-if [[ "$VIRTUAL_ENV" == "" ]]; then
+venv() {
     if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
         echo "Run this command as source"
         exit 1
     else
-        setup_venv
-    fi
-fi
+        python3 -m venv venv
+        source venv/bin/activate
 
-build_rust
+        install_dep
+
+        # Re-activate for pytest
+        deactivate && source venv/bin/activate
+    fi
+}
+
+rust() {
+    maturin develop --manifest-path crates/pytohdl/Cargo.toml
+}
+
+ci() {
+    dep
+    rust
+}
+
+"$@"
