fac17fc1c0fdaeb88878c2f919f514d6834e8540
Kerry Wang <kerrywang369@gmail.com>
2023-12-19 13:16:01 -0700

    testing new build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
-#! /bin/bash
-mkdir build -p && cd build && python3 -m pip install .. && cd ..
+#! /bin/bash -ex
+setup_venv() {
+    python3 -m venv venv
+    source venv/bin/activate
+    python3 -m pip install -e .[dev]
+
+    # Re-activate for pytest
+    deactivate && source venv/bin/activate
+}
+
+build_rust() {
+    maturin develop --manifest-path crates/pytohdl/Cargo.toml
+}
+
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+    if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+        echo "Run this command as source"
+        exit 1
+    else
+        setup_venv
+    fi
+fi
+
+build_rust
