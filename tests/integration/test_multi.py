bb9431a274e6a8858d164243f414389f0a7539a2
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2024-04-04 22:34:44 -0600

    Make pytest not take in an optimization level argument (#60)
    - Consolidates arguments into a new class
    - Enables specifying optimization level for each test case in source
    code
    - Changes pytest CLI argument to be a limit on optimization levels being
    tested

--- tests/integration/test_multi.py
+++ tests/integration/test_multi.py
@@ -2,28 +2,56 @@
 Test suite for functions that call other functions
 """
 
-from types import FunctionType
-from typing import Iterable, Union
-
 import pytest
 from parameterized import parameterized
 
 from .base_tests import BaseTestWrapper
 from .functions import *
-from .utils import name_func
+from .utils import Parameters, name_func
 
 PARAMETERS = [
-    ([multi_funcs, multiplier, p2vrange], [(13, 17)]),
-    ([fib_product, multiplier, fib, p2vrange], [10, 20]),
-    ([fib_product, multiplier, fib, p2vrange], [10, 20]),
-    ([fib, p2vrange], range(10, 31, 10)),
-    ([quad_multiply, multiplier_generator], [(3, 7), (31, 43)]),
-    ([double_for, p2vrange], [5, 10, 15, 20]),
-    ([dupe, p2vrange], [(0, 10, 1), (3, 73, 7)]),
-    ([olympic_logo_naive, circle_lines], [(10, 10, 4), (13, 13, 7)]),
-    (
-        [olympic_logo, olympic_logo_mids, circle_lines],
-        [(10, 10, 4), (13, 13, 7)],
+    Parameters(
+        func=multi_funcs,
+        helpers=[multiplier, p2vrange],
+        args_list=[(13, 17)],
+    ),
+    Parameters(
+        func=fib_product,
+        helpers=[multiplier, fib, p2vrange],
+        args_list=[10, 20],
+    ),
+    Parameters(
+        func=fib,
+        helpers=[p2vrange],
+        args_list=range(10, 31, 10),
+    ),
+    Parameters(
+        func=quad_multiply,
+        helpers=[multiplier_generator],
+        args_list=[
+            (3, 7),
+            (31, 43),
+        ],
+    ),
+    Parameters(
+        func=double_for,
+        helpers=[p2vrange],
+        args_list=[5, 10, 15, 20],
+    ),
+    Parameters(
+        func=dupe,
+        helpers=[p2vrange],
+        args_list=[(0, 10, 1), (3, 73, 7)],
+    ),
+    Parameters(
+        func=olympic_logo_naive,
+        helpers=[circle_lines],
+        args_list=[(10, 10, 4), (13, 13, 7)],
+    ),
+    Parameters(
+        func=olympic_logo,
+        helpers=[olympic_logo_mids, circle_lines],
+        args_list=[(10, 10, 4), (13, 13, 7)],
     ),
 ]
 
@@ -34,23 +62,15 @@ class TestMulti(BaseTestWrapper.BaseTest):
         input=PARAMETERS,
         name_func=name_func,
     )
-    def test_perf(
-        self,
-        funcs: Iterable[FunctionType],
-        test_cases: Iterable[Union[tuple[int, ...], int]],
-    ):
-        BaseTestWrapper.BaseTest.test_perf(self, funcs, test_cases)
+    def test_perf(self, test_param: Parameters):
+        BaseTestWrapper.BaseTest.test_perf(self, test_param)
 
     @parameterized.expand(
         input=PARAMETERS,
         name_func=name_func,
     )
-    def test_correct(
-        self,
-        funcs: Iterable[FunctionType],
-        test_cases: Iterable[Union[tuple[int, ...], int]],
-    ):
-        BaseTestWrapper.BaseTest.test_correct(self, funcs, test_cases)
+    def test_correct(self, test_param: Parameters):
+        BaseTestWrapper.BaseTest.test_correct(self, test_param)
 
     @classmethod
     def tearDownClass(cls):
--- tests/integration/test_single.py
+++ tests/integration/test_single.py
@@ -2,29 +2,63 @@
 Test suite for basic functions
 """
 
-from types import FunctionType
-from typing import Union
-
 import pytest
 from parameterized import parameterized
 
 from .base_tests import BaseTestWrapper
 from .functions import *
-from .utils import name_func
+from .utils import Parameters, name_func
 
 PARAMETERS = [
-    (keyword_test, [()]),
-    (floor_div, [13, 23]),
-    (operators, [(31, 13), (-31, 13), (31, -13), (-31, -13)]),
-    (multiplier_generator, [(13, 17), (78, 67), (15, -12)]),
-    (multiplier, [(13, 17), (78, 67), (15, -12)]),
-    (p2vrange, [(0, 10, 1), (0, 1000, 1)]),
-    (division, [(6, 7, 10), (2, 3, 30), (13, 17, 5)]),
-    (circle_lines, [(21, 37, 7), (79, 45, 43)]),
-    (happy_face, [(50, 51, 7), (76, 97, 43)]),
-    (rectangle_filled, [(32, 84, 5, 7), (64, 78, 23, 27)]),
-    (rectangle_lines, [(32, 84, 5, 7), (84, 96, 46, 89)]),
-    (floating_point_add, [(0, 127, 0, 0, 128, 0)]),  # 1 + 2
+    Parameters(
+        func=keyword_test,
+        args_list=[()],
+    ),
+    Parameters(
+        func=floor_div,
+        args_list=[13, 23],
+    ),
+    Parameters(
+        func=operators,
+        args_list=[(31, 13), (-31, 13), (31, -13), (-31, -13)],
+    ),
+    Parameters(
+        func=multiplier_generator,
+        args_list=[(13, 17), (78, 67), (15, -12)],
+    ),
+    Parameters(
+        func=multiplier,
+        args_list=[(13, 17), (78, 67), (15, -12)],
+    ),
+    Parameters(
+        func=p2vrange,
+        args_list=[(0, 10, 1), (0, 1000, 1)],
+    ),
+    Parameters(
+        func=division,
+        args_list=[(6, 7, 10), (2, 3, 30), (13, 17, 5)],
+    ),
+    Parameters(
+        func=circle_lines,
+        args_list=[(21, 37, 7), (79, 45, 43)],
+    ),
+    Parameters(
+        func=happy_face,
+        args_list=[(50, 51, 7), (76, 97, 43)],
+    ),
+    Parameters(
+        func=rectangle_filled,
+        args_list=[(32, 84, 5, 7), (64, 78, 23, 27)],
+    ),
+    Parameters(
+        func=rectangle_lines,
+        args_list=[(32, 84, 5, 7), (84, 96, 46, 89)],
+    ),
+    Parameters(
+        func=floating_point_add,
+        args_list=[(0, 127, 0, 0, 128, 0)],  # 1 + 3
+        opti_levels=Parameters.zero_and_exp2s(2),
+    ),
 ]
 
 
@@ -34,19 +68,15 @@ class TestSingle(BaseTestWrapper.BaseTest):
         input=PARAMETERS,
         name_func=name_func,
     )
-    def test_perf(
-        self, funcs: list[FunctionType], test_cases: list[Union[tuple[int, ...], int]]
-    ):
-        BaseTestWrapper.BaseTest.test_perf(self, funcs, test_cases)
+    def test_perf(self, test_param: Parameters):
+        BaseTestWrapper.BaseTest.test_perf(self, test_param)
 
     @parameterized.expand(
         input=PARAMETERS,
         name_func=name_func,
     )
-    def test_correct(
-        self, funcs: list[FunctionType], test_cases: list[Union[tuple[int, ...], int]]
-    ):
-        BaseTestWrapper.BaseTest.test_correct(self, funcs, test_cases)
+    def test_correct(self, test_param: Parameters):
+        BaseTestWrapper.BaseTest.test_correct(self, test_param)
 
     @classmethod
     def tearDownClass(cls):
