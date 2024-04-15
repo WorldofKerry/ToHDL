c79691d841fe44906d0b9cf899ac1d5f4f6a12c8
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2024-04-14 21:53:34 -0700

    Fixed mixed rust/python build system (#78)
    Change the build files to follow what maturin recommends for a mixed
    Python/Rust project https://www.maturin.rs/project_layout

---
+++ python2verilog/pytohdl.pyi
@@ -0,0 +1,13 @@
+# pylint: skip-file
+"""
+Python to Verilog translater based on ToHDL
+"""
+
+class PyContext:
+    """
+    Context for a Python function and all of its called upon functions
+    """
+
+    def __init__(self, main: str, functions: dict[str, str]): ...
+
+def translate(context: PyContext) -> str: ...
