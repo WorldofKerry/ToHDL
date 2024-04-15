002110c2f6c3309ee0ae2ad97def2c167ca5622e
Kerry Wang <kerrywang369@gmail.com>
2024-04-14 19:30:15 -0700

    update stub, ignore pyi in pylint

--- python2verilog/pytohdl.pyi
+++ python2verilog/pytohdl.pyi
@@ -1,4 +1,12 @@
+"""
+Python to Verilog translater based on ToHDL
+"""
+
 class PyContext:
+    """
+    Context for a Python function and all of its called upon functions
+    """
+
     def __init__(self, main: str, functions: dict[str, str]): ...
 
 def translate(context: PyContext) -> str: ...
