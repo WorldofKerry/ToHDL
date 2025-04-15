c18ec82d557c3cf47d581a6bedddbd4dc19e1427
Kerry Wang <kerrywang369@gmail.com>
2025-04-15 15:01:47 -0700

    update pyo3 version, and address fixes

--- crates/pytohdl/src/lib.rs
+++ crates/pytohdl/src/lib.rs
@@ -16,7 +16,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn pytohdl(_py: Python, m: &PyModule) -> PyResult<()> {
+fn pytohdl(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(translate, m)?)?;
     m.add_function(wrap_pyfunction!(python_to_python_fsm, m)?)?;
